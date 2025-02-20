b052bdbe1beba22cdbe88e700f1386685ff1ceb0
wangzongming <1746809408@qq.com>
2025-02-20 21:30:43 +0800

    update

--- index.js
+++ index.js
@@ -8,27 +8,42 @@ module.exports = {
     name: "esp-ai-plugin-llm-example",
     // 插件类型 LLM | TTS | IAT
     type: "LLM",
-    /**
-     * 大语言模型插件
-     * @param {String}      device_id           设备id 
-     * @param {Number}      devLog              日志输出等级，为0时不应该输出任何日志   
-     * @param {Object}      api_key             用户配置的key   
-     * @param {String}      text                对话文本
-     * @param {Function}    cb                  LLM 服务返回音频数据时调用，eg: cb({ text, texts })
-     * @param {Function}    llmServerErrorCb    与 LLM 服务之间发生错误时调用，并且传入错误说明，eg: llmServerErrorCb("意外错误")
-     * @param {Function}    llm_params_set      用户配置的设置 LLM 参数的函数
-     * @param {Function}    logWSServer         将 ws 服务回传给框架，如果不是ws服务可以这么写: logWSServer({ close: ()=> {} })
-     * @param {{role, content}[]}  llm_init_messages   用户配置的初始化时的对话数据
-     * @param {{role, content}[]}  llm_historys        llm 历史对话数据
-     * @param {Function}    log                 为保证日志输出的一致性，请使用 log 对象进行日志输出，eg: log.error("错误信息")、log.info("普通信息")、log.llm_info("llm 专属信息")
-     *  
-    */
-    main({ device_id, devLog, api_key, text, llmServerErrorCb, llm_init_messages = [], llm_historys = [], cb, llm_params_set, logWSServer }) {
+/**
+ * 大语言模型插件
+ * @param {String}      device_id           设备id 
+ * @param {Number}      devLog              日志输出等级，为0时不应该输出任何日志   
+ * @param {Object}      llm_config          用户配置的 apikey 等信息   
+ * @param {String}      iat_server          用户配置的 iat 服务 
+ * @param {String}      llm_server          用户配置的 llm 服务 
+ * @param {String}      tts_server          用户配置的 tts 服务 
+ * @param {String}      text                对话文本
+ * @param {Function}    connectServerBeforeCb 连接 LLM 服务逻辑开始前需要调用这个方法告诉框架：eg: connectServerBeforeCb()
+ * @param {Function}    connectServerCb     连接 LLM 服务后需要调用这个方法告诉框架：eg: connectServerCb(true)
+ * @param {Function}    cb                  LLM 服务返回音频数据时调用，eg: cb({  count_text, text, texts })
+ * @param {Function}    llmServerErrorCb    与 LLM 服务之间发生错误时调用，并且传入错误说明，eg: llmServerErrorCb("意外错误")
+ * @param {Function}    llm_params_set      用户配置的设置 LLM 参数的函数
+ * @param {Function}    logWSServer         将 ws 服务回传给框架，如果不是ws服务可以这么写: logWSServer({ close: ()=> {  中断逻辑... } })
+ * @param {{role, content}[]}  llm_init_messages   用户配置的初始化时的对话数据
+ * @param {{role, content}[]}  llm_historys llm 历史对话数据
+ * @param {Function}    log                 为保证日志输出的一致性，请使用 log 对象进行日志输出，eg: log.error("错误信息")、log.info("普通信息")、log.llm_info("llm 专属信息")
+ *  
+*/
+    main({ devLog, is_pre_connect, llm_config, text, llmServerErrorCb, llm_init_messages = [], llm_historys = [], cb, llm_params_set, logWSServer, connectServerBeforeCb, connectServerCb, log }) {
 
         devLog && console.log("对话记录：\n", llm_historys)
 
         // 请自行约定接口 key 需要配置什么字段
-        const config = { ...api_key }
+        const config = { ...llm_config }
+        
+        
+        // 预先连接函数
+        async function preConnect() {
+            // some code...
+        }
+        if (is_pre_connect) {
+            preConnect()
+            return;
+        }
 
         // 连接 ws 服务后并且上报给框架
         // const llm_ws = new WebSocket("ws://xxx");
